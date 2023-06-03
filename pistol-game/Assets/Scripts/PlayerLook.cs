59a5c5456f48ca427cf416d0f85b3b80bc677afc
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2023-06-03 21:48:42 +0100

    created player controller
    look, move, jump

---
+++ pistol-game/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+    public float ySensitivity = 10f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        // calculate rotation for up and down
+        xRotation -= mouseY * ySensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        transform.Rotate(Vector3.up * (mouseX * xSensitivity * Time.deltaTime));
+    }
+}
